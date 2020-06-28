88341361b046989c21cc2876a12e5bb8d1bfdf82
lsqls <764666894@qq.com>
2020-06-28 12:09:01 +0800

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -2,15 +2,15 @@
 import requests
 import os
 from lxml import etree
-max=10000;min=4000;sendemail="0";info=''
+max=10000;min=4000;sendemail=False;info=''
 url='https://price.btcfans.com/'
 html = etree.HTML(requests.get(url).text)
 price = float(html.xpath('//li[@id="coin-bitcoin"]//span[@class="last-price"]/text()')[0].replace(',',''))
 if (price>max):
-    sendemail="1"
+    sendemail=True
     info="The current bitcoin price is ${0}, it is recommended to sell".format(price)
 if (price<min):
-    sendemail="1"
+    sendemail=True
     info="The current bitcoin price is ${0}, it is recommended to buy".format(price)
-os.environ['SENDMAIL']=sendemail
-os.environ['INFO']=info
+print("::set-env name=sendemail::{}".format(sendemail))
+print("::set-env name=info::{}".format(info))
